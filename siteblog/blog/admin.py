02081e1cdff5fc30c82051af4c8e9a1419ec6bbb
Yurii <yura.onyshchuk@gmail.com>
2022-01-31 09:34:16 +0200

    Add 'prepopulated_fields' for auto create 'slug'

--- siteblog/blog/admin.py
+++ siteblog/blog/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Category)
-admin.site.register(Tag)
-admin.site.register(Post)
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Post, PostAdmin)
