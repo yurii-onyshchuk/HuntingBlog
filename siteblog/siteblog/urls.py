25b5a525ce19fb3d37c2a444fc4257706bab6701
Yurii <yura.onyshchuk@gmail.com>
2022-01-31 18:39:13 +0200

    Add 'urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)' for serve the static media from Django when DEBUG = True

--- siteblog/siteblog/urls.py
+++ siteblog/siteblog/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,6 +25,5 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += [
-        path('__debug__/', include('debug_toolbar.urls')),
-    ]
+    urlpatterns += [path('__debug__/', include('debug_toolbar.urls'))]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
