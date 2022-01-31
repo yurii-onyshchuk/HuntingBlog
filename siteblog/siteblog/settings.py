acd5597da3bb6654f85070c2b8e934e36ad7ae07
Yurii <yura.onyshchuk@gmail.com>
2022-01-31 16:18:03 +0200

    Add 'MEDIA_URL, MEDIA_ROOT'

--- siteblog/siteblog/settings.py
+++ siteblog/siteblog/settings.py
@@ -115,8 +115,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'siteblog/static'),
-                    ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'siteblog/static'),]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
