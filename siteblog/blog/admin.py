77b3efb625eabd65e7f2c698ec664ca7681e298b
Yurii <yura.onyshchuk@gmail.com>
2022-02-01 20:29:27 +0200

    Setting admin panel: minor edit

--- siteblog/blog/admin.py
+++ siteblog/blog/admin.py
@@ -26,11 +26,11 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}  # Auto create of Slug
     form = PostAdminForm  # For ckeditor
     save_on_top = True
-    list_display = ('id', 'title', 'category', 'created_at', 'views', 'get_photo',)
+    list_display = ('id', 'title', 'category', 'created_at', 'views', 'get_photo', 'author',)
     list_display_links = ('id', 'title',)
-    list_filter = ('category',)
+    list_filter = ('category', 'author', )
     search_fields = ('title',)
-    fields = ('title', 'slug', 'category', 'tags', 'content', 'get_photo', 'views', 'created_at',)
+    fields = ('title', 'slug', 'author', 'category', 'tags', 'content', 'get_photo', 'views', 'created_at',)
     readonly_fields = ('created_at', 'views', 'get_photo',)
 
     def get_photo(self, obj):
--- siteblog/blog/models.py
+++ siteblog/blog/models.py
@@ -28,15 +28,15 @@ class Tag(models.Model):
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=250, verbose_name='Заголовок статті')
+    title = models.CharField(max_length=250, verbose_name='Заголовок')
     slug = models.SlugField(max_length=100, verbose_name='URL', unique=True)
     category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts', verbose_name='Категорія')
-    author = models.CharField(max_length=100, verbose_name='Автор статті')
+    author = models.CharField(max_length=100, verbose_name='Автор')
     content = models.TextField(blank=True, verbose_name='Вміст')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публікації')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True, verbose_name='Фото')
     tags = models.ManyToManyField(Tag, blank=True, related_name='posts', verbose_name='Теги')
-    views = models.IntegerField(default=0, verbose_name='Кількість переглядів')
+    views = models.IntegerField(default=0, verbose_name='Перегляди')
 
     def __str__(self):
         return self.title
