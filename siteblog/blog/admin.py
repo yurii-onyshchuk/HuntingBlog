c9c15f6dd524bd1e81702422d39c85523465f78f
Yurii <yura.onyshchuk@gmail.com>
2022-02-01 16:09:39 +0200

    Setting admin panel: verbose_name, verbose_name_plural, __str__

--- siteblog/blog/admin.py
+++ siteblog/blog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import *
 from django import forms
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
@@ -21,8 +23,15 @@ class TagAdmin(admin.ModelAdmin):
 
 
 class PostAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("title",)}
-    form = PostAdminForm
+    prepopulated_fields = {"slug": ("title",)}  # Auto create of Slug
+    form = PostAdminForm  # For ckeditor
+    save_on_top = True
+    list_display = ('id', 'title', 'category', 'created_at', 'views', 'photo',)
+    list_display_links = ('id', 'title',)
+    list_filter = ('category',)
+    search_fields = ('title',)
+    fields = ('title', 'slug', 'category', 'tags', 'content', 'photo', 'views', 'created_at',)
+    readonly_fields = ('created_at', 'views', 'photo',)
 
 
 admin.site.register(Category, CategoryAdmin)
--- siteblog/blog/models.py
+++ siteblog/blog/models.py
@@ -28,20 +28,20 @@ class Tag(models.Model):
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=250, verbose_name='Заголовок посту')
+    title = models.CharField(max_length=250, verbose_name='Заголовок статті')
     slug = models.SlugField(max_length=100, verbose_name='URL', unique=True)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts')
-    author = models.CharField(max_length=100, verbose_name='Автор посту')
-    content = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts', verbose_name='Категорія')
+    author = models.CharField(max_length=100, verbose_name='Автор статті')
+    content = models.TextField(blank=True, verbose_name='Вміст')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публікації')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True, verbose_name='Фото')
-    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
+    tags = models.ManyToManyField(Tag, blank=True, related_name='posts', verbose_name='Теги')
     views = models.IntegerField(default=0, verbose_name='Кількість переглядів')
 
     def __str__(self):
         return self.title
 
     class Meta:
-        verbose_name = 'Пост'
-        verbose_name_plural = 'Пости'
+        verbose_name = 'Стаття(ю)'
+        verbose_name_plural = 'Статті'
         ordering = ['-created_at']
