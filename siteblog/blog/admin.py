bc2c55098b16f15f1ca0ac7ccbb94b7b00795d79
Yurii <yura.onyshchuk@gmail.com>
2022-02-01 20:14:55 +0200

    Setting admin panel: add photo miniature

--- siteblog/blog/admin.py
+++ siteblog/blog/admin.py
@@ -26,12 +26,19 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}  # Auto create of Slug
     form = PostAdminForm  # For ckeditor
     save_on_top = True
-    list_display = ('id', 'title', 'category', 'created_at', 'views', 'photo',)
+    list_display = ('id', 'title', 'category', 'created_at', 'views', 'get_photo',)
     list_display_links = ('id', 'title',)
     list_filter = ('category',)
     search_fields = ('title',)
-    fields = ('title', 'slug', 'category', 'tags', 'content', 'photo', 'views', 'created_at',)
-    readonly_fields = ('created_at', 'views', 'photo',)
+    fields = ('title', 'slug', 'category', 'tags', 'content', 'get_photo', 'views', 'created_at',)
+    readonly_fields = ('created_at', 'views', 'get_photo',)
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<a href="{obj.photo.url}"><img src="{obj.photo.url}" width="80"></a>')
+        return '-'
+
+    get_photo.short_description = 'Фото'
 
 
 admin.site.register(Category, CategoryAdmin)
