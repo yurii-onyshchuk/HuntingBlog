779eab0c6345e5961efd9d14f7f517b179f3941d
Yurii <yura.onyshchuk@gmail.com>
2022-02-06 23:05:46 +0200

    Posts in home page with ListView class

--- siteblog/blog/urls.py
+++ siteblog/blog/urls.py
@@ -17,7 +17,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.Home.as_view(), name='home'),
     path('category/<str:slug>', views.get_category, name='category'),
     path('post/<str:slug>', views.get_post, name='post')
 ]
--- siteblog/blog/views.py
+++ siteblog/blog/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post, Category, Tag
 
 
-def index(request):
-    return render(request, 'blog/index.html')
+class Home(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Classic Blog Design'
+        return context
 
 
-def get_category(request):
+def get_category(request, slug):
     return render(request, 'blog/category.html')
 
 
-def get_post(request):
+def get_post(request, slug):
     return render(request, 'blog/post.html')
