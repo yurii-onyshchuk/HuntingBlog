4f4423b18dde0ef0a04d5955a5c6414526b8f04f
Yurii <yura.onyshchuk@gmail.com>
2022-03-08 07:51:44 +0200

    _random.choice of post in post list by tag

--- siteblog/blog/views.py
+++ siteblog/blog/views.py
@@ -47,6 +47,7 @@ class PostsByTag(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = Tag.objects.get(slug=self.kwargs['slug'])
+        context['random_post'] = random.choice(self.get_queryset())
         return context
 
 
