0404bf26605da14930058965ffe0ee53da57389c
Yurii Onyshchuk <yura.onyshchuk@gmail.com>
2022-04-21 19:25:32 +0300

    Change general project structure

--- HuntingBlog/asgi.py
+++ HuntingBlog/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for siteblog project.
+ASGI config for HuntingBlog project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'siteblog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HuntingBlog.settings')
 
 application = get_asgi_application()
--- HuntingBlog/wsgi.py
+++ HuntingBlog/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for siteblog project.
+WSGI config for HuntingBlog project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'siteblog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HuntingBlog.settings')
 
 application = get_wsgi_application()
