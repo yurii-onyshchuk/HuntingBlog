4a06892d700d160a4e7fba1b0b9966cc077dcee7
Yurii Onyshchuk <yura.onyshchuk@gmail.com>
2022-12-12 21:39:51 +0200

    Added hiding of empty categories and tags

--- blog/templatetags/sidebar.py
+++ blog/templatetags/sidebar.py
@@ -1,4 +1,6 @@
 from django import template
+from django.db.models import Count
+
 from blog.models import Post, Tag, Category
 
 register = template.Library()
@@ -13,9 +15,9 @@ def show_posts_list(count=5):
 
 @register.simple_tag()
 def show_tag_list():
-    return Tag.objects.all()
+    return Tag.objects.annotate(num_posts=Count('posts')).filter(num_posts__gt=0)
 
 
 @register.simple_tag()
 def get_categories():
-    return Category.objects.all()
+    return Category.objects.annotate(num_posts=Count('posts')).filter(num_posts__gt=0)
